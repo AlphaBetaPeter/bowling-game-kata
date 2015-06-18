113fe90313168c32d0d954a2852594436fdd0926
Peter Mösenthin <moesenthin@sipgate.de>
2015-06-18 16:40:52 +0200

    add python version

---
+++ python/bowling-game-kata/bowling_game_kata.py
@@ -0,0 +1,39 @@
+class BowlingGameKata():
+
+	def __init__(self):
+		self.rolls = [0] * 21
+		self.current_roll = 0
+
+	def roll(self, pins):
+		self.rolls[self.current_roll] = pins
+		self.current_roll += 1
+
+	def score(self):
+		score = 0
+		frame_index = 0
+		for frame in range(10):
+			if self.is_strike(frame_index):
+				score += 10 + self.strike_bonus(frame_index)
+				frame_index += 1
+			elif self.is_spare(frame_index):
+				score += 10 + self.spare_bonus(frame_index)
+				frame_index += 2
+			else:
+				score += self.sum_of_balls_in_frame(frame_index)
+				frame_index += 2
+		return score
+
+	def is_spare(self, frame_index):
+		return self.rolls[frame_index] + self.rolls[frame_index + 1] == 10
+
+	def is_strike(self, frame_index):
+		return self.rolls[frame_index] == 10
+
+	def sum_of_balls_in_frame(self, frame_index):
+		return self.rolls[frame_index] + self.rolls[frame_index + 1]
+
+	def spare_bonus(self, frame_index):
+		return self.rolls[frame_index + 2]
+
+	def strike_bonus(self, frame_index):
+		return self.rolls[frame_index + 1] + self.rolls[frame_index + 2]
